351c8a275b05c4581e3c389e6f580ea243271a0f
ssrividya26 <S.Srividya@cognizant.com>
2026-05-12 17:45:19 +0530

    api gateway uploadedd

---
+++ api-gateway/src/test/java/com/cts/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cts.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
