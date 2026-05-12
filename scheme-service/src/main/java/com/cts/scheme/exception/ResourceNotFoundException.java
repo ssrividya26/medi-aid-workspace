afc61688ff91ed1e94ba8b5729798ef90cd79f41
ssrividya26 <S.Srividya@cognizant.com>
2026-05-12 17:55:09 +0530

    Delete scheme-service directory

--- scheme-service/src/main/java/com/cts/scheme/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.cts.scheme.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
--- scheme-service/src/main/java/com/cts/scheme/repository/SchemeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cts.scheme.repository;
-
-import com.cts.scheme.model.Scheme;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchemeRepository extends JpaRepository<Scheme,Long> {
-}
